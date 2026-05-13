90e975600cc82cb91b7d718d829bf0d206eafbdf
Alex Garcia <alexsebastian.garcia@gmail.com>
2026-05-13 08:23:04 -0700

    old sidebar stuff

--- datasette_scribe/__init__.py
+++ datasette_scribe/__init__.py
@@ -54,3 +54,28 @@ def database_actions(datasette, actor, database):
         return []
 
     return inner
+
+
+try:
+    from datasette_sidebar.hookspecs import SidebarApp
+    # TODO clean this up
+    def _first_visible_db(datasette):
+        for name in datasette.databases:
+            if name != "_internal":
+                return name
+        return "_internal"
+
+    @hookimpl
+    def datasette_sidebar_apps(datasette):
+        return [
+            SidebarApp(
+                label="Scribe",
+                description="Audio transcription manager",
+                href=lambda db: f"/{db}/-/scribe" if db else f"/{_first_visible_db(datasette)}/-/scribe",
+                icon='<svg viewBox="0 0 16 16" fill="currentColor"><path d="M8 1a3 3 0 0 0-3 3v4a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"/><path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V14.5h2a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1h2v-1.525A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/></svg>',
+                color="#6c63ff",
+            ),
+        ]
+
+except ImportError:
+    pass
